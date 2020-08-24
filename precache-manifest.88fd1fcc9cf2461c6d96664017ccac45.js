bc1c89bb7f2f2b3fa197f4fd98edba46c4f184f9
evil_liskov <evil_liskov@protonmail.com>
2020-08-24 08:18:24 +0000

    Updates

---
+++ precache-manifest.88fd1fcc9cf2461c6d96664017ccac45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd15554eb651f6fdd70e7b0889de976b",
+    "url": "/terminal/index.html"
+  },
+  {
+    "revision": "65fd6232cc8fff22c7b1",
+    "url": "/terminal/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "81e7d137a64b285efb53",
+    "url": "/terminal/static/js/2.2827543d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/terminal/static/js/2.2827543d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65fd6232cc8fff22c7b1",
+    "url": "/terminal/static/js/main.ff93ff0d.chunk.js"
+  },
+  {
+    "revision": "f8b0b01685f9252a3d90",
+    "url": "/terminal/static/js/runtime-main.437cc026.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/terminal/static/media/logo.5d5d9eef.svg"
+  }
+]);
